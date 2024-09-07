27976f3b483edfb55ccf11f04674428ebba0f30f
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-09-07 16:41:12 +0530

    added auth services along eith tooken generation, verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const { handleUserSignup, handleUserLogin } = require('../controller/user');
+const express = require("express");
+const { handleUserSignup, handleUserLogin } = require("../controller/user");
+const {
+  loginValidator,
+  signupValidator,
+  validate,
+} = require("../utils/validator");
 const router = express.Router();
 
 // Create a new user
-router.post('/signup', handleUserSignup);
-router.post('/login', handleUserLogin);
+router.post("/signup", validate(signupValidator), handleUserSignup);
+router.post("/login", validate(loginValidator), handleUserLogin);
 
 
 module.exports = router;
