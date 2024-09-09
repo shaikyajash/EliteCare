9aea89dc46c49ca1ac2bb40b46929552e53f722e
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-09-09 23:50:00 +0530

    added appointment route and saved it in db

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,6 +1,16 @@
-const express = require('express');
+const express = require("express");
+const { handleAppointment } = require("../controller/appointment");
+const { appointmentValidator, validate } = require("../utils/validator");
+const { verifyToken } = require("../utils/tokenManager");
+
 const router = express.Router();
 
-// Create a new user
+// api/appointments/book
+router.post(
+  "/book",
+  validate(appointmentValidator),
+  verifyToken,
+  handleAppointment
+);
 
 module.exports = router;
