b503ed15377b4dfacf7c8ca048d15ac97f0731e9
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-09-17 22:04:25 +0530

    commit

---
+++ sih/src/Component/ChatBot/Bot.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState, useRef, useContext } from "react";
+import { Button } from "@nextui-org/button";
+import {
+  FiMoreVertical,
+  FiArrowLeft,
+  FiPaperclip,
+  FiSend,
+} from "react-icons/fi";
+const Bubble = ({ text, time, index, image }) => {
+  const alignmentClass = index % 2 === 0 ? "justify-start" : "justify-end";
+
+  return (
+    <div className={`flex items-center gap-2 ${alignmentClass}`}>
+      <div className="flex items-start ">
+        {index % 2 === 0 && (
+          <button className="m-1 p-2 bg-[#ed4d4d] rounded-xl text-sm hover:bg-slate-300"></button>
+        )}
+        <div className="bg-[#e6e6e6] p-2 m-2 rounded-xl">
+          {image && <img src={image} alt="image" className="w-40" />}
+          <p className="max-w-[40vw]">{text}</p>
+          <p className="text-xs text-end">{time}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function Bot() {
+  const [chatMessages, setChatMessages] = useState(() => {
+    const savedMessages = localStorage.getItem("chatMessages");
+    return savedMessages ? JSON.parse(savedMessages) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("chatMessages", JSON.stringify(chatMessages));
+  }, [chatMessages]);
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+  useEffect(scrollToBottom, [chatMessages]);
+
+  const [newMessage, setNewMessage] = useState({
+    text: "",
+    time: "",
+    image: "",
+  });
+  const sendMessage = async () => {
+    newMessage.time = new Date().toLocaleTimeString();
+    setChatMessages([...chatMessages, newMessage]);
+    setNewMessage({ text: "" });
+  };
+
+  const handlePicker = () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        newMessage.image = reader.result;
+        sendMessage();
+      };
+    };
+    input.click();
+  };
+  const Pop = () => {
+    return (
+      <div>
+        <button onClick={handlePicker} className="bg-red-500 rounded-full p-2">
+          <FiPaperclip />
+        </button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="h-screen w-[450px]">
+      <div className="flex items-center justify-between h-12 border-b border-black p-2">
+        <div className="flex items-center gap-2">
+          <FiArrowLeft />
+          <div className="bg-red-500 rounded-full h-8 w-8"></div>
+          <p className="text-xl">Bot</p>
+        </div>
+        <FiMoreVertical />
+      </div>
+      <div className="flex flex-col overflow-y-auto h-[calc(100vh-6rem)] bg-gray-100 p-4">
+        {chatMessages.map((message, index) => (
+          <Bubble
+            key={index}
+            text={message.text}
+            time={message.time}
+            image={message.image}
+            index={index}
+          />
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+
+      <div className=" border-t border-black fixed bottom-0 w-full flex items-center justify-between p-2 gap-2 bg-white">
+        <div className="flex flex-row items-center justify-between gap-2">
+          <Pop />
+          <input
+            type="text"
+            placeholder="Enter here..."
+            value={newMessage.text}
+            onChange={(e) => setNewMessage({ text: e.target.value })}
+            className="bg-[#d7d5d5] rounded-xl p-2 w-[77vw]"
+          />
+        </div>
+        <Button
+          onClick={sendMessage}
+          isIconOnly
+          className="bg-red-500 p-2 rounded-full"
+        >
+          <FiSend />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Bot;
--- sih/src/index.js
+++ sih/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { UserProvider } from './UserContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { UserProvider } from "./UserContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+      <UserProvider>
+        <App />
+      </UserProvider>
   </React.StrictMode>
 );
-
