eb0f98293e9b22b7c378c4ca51e16f368e181193
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-09-18 03:40:06 +0530

    table

--- backend/routes/ngo.js
+++ backend/routes/ngo.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const fs = require("fs");
+const csv = require("csv-parser");
 
 const router = express.Router();
 
-
 router.get("/", (req, res) => {
-    res.json({ msg: "Route is working", status: "T", data: null });
-  });
-  
+  const results = [];
+
+  fs.createReadStream("./ngo_data.csv")
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      // Transform the results array
+      const transformedResults = results.map((record) => {
+        const transformedRecord = { ...record };
+        const firstKey = Object.keys(record)[0];
+        const firstValue = record[firstKey];
+        transformedRecord.Name = firstValue; // Add new key with the value of the first key
+        return transformedRecord;
+      });
+
+      res.json(transformedResults);
+    });
+});
 
 module.exports = router;
