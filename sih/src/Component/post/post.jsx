8b3881845424f4691ca06396d2f9430805ba7426
GRASiN1 <gauravrajsingh047@gmail.com>
2024-09-18 03:49:00 +0530

    fixed alingment in post page

--- sih/src/Component/post/post.jsx
+++ sih/src/Component/post/post.jsx
@@ -9,7 +9,7 @@ export default function Post() {
     return(
         <div className="'bg-gray-100 min-h-screen w-full">
             <Navbar/>
-            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-16'>
                 <div className='bg-white p-4 rounded-lg shadow-green-500 shadow flex flex-col items-start'>
                     <div className='flex flex-row items-start w-full '>
                         <img src='/images/avatar.png' alt='avatar' className='w-10 h-10 rounded-full mr-4'/>
